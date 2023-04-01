77bdec4ecc9e7e27a1915c85e762a9c9a140f990
Abhilash4597 <abhilash4597@gmail.com>
2023-04-01 13:20:07 +0530

    design for list view done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
+  function changeLayout() {
+    let customStyle = {
+      display: 'block',
+      width: '80%',
+    };
+  }
+
   return (
     <div>
       <div className="header">
         <h1>Photo Gallery</h1>
         <div className="header-select">
-          <select name="" id="header-select">
-            <option value="">Select</option>
-            <option value="">List</option>
-            <option value="">Grid</option>
-          </select>
+          <form action="#">
+            <label htmlFor="header-select"></label>
+            <select name="header-select" id="header-select">
+              <option value="" disabled>
+                Select
+              </option>
+              <option value="List">List</option>
+              <option value="Grid">Grid</option>
+            </select>
+          </form>
         </div>
       </div>
     </div>
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -18,107 +18,107 @@ export default function List() {
     {
       id: 1,
       img: 'https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636__340.jpg',
-      title: 'elephant trunk tusks',
+      title: 'Elephant trunk tusks',
     },
     {
       id: 2,
       img: 'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267__340.jpg',
-      title: 'owl ,bird of prey.',
+      title: 'Owl ,bird of prey.',
     },
     {
       id: 3,
       img: 'https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445__340.jpg',
-      title: 'iceland arctic fox',
+      title: 'Iceland arctic fox',
     },
     {
       id: 4,
       img: 'https://cdn.pixabay.com/photo/2018/08/12/16/59/parrot-3601194__340.jpg',
-      title: 'parrot yellow macaw',
+      title: 'Parrot yellow macaw',
     },
     {
       id: 5,
       img: 'https://cdn.pixabay.com/photo/2016/03/27/22/22/fox-1284512__340.jpg',
-      title: 'fox sleeping resting',
+      title: 'Fox sleeping resting',
     },
     {
       id: 6,
       img: 'https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg',
-      title: 'flowers butterfly',
+      title: 'Flowers butterfly',
     },
     {
       id: 7,
       img: 'https://cdn.pixabay.com/photo/2017/05/08/13/15/bird-2295431__340.jpg',
-      title: 'bird branch perched',
+      title: 'Bird branch perched',
     },
     {
       id: 8,
       img: 'https://cdn.pixabay.com/photo/2016/01/02/16/53/lion-1118467__340.jpg',
-      title: 'lion head face mane',
+      title: 'Lion head face mane',
     },
     {
       id: 9,
       img: 'https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247__340.jpg',
-      title: 'sea turtle diving',
+      title: 'Sea turtle diving',
     },
     {
       id: 10,
       img: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg',
-      title: 'cat young animal kitten',
+      title: 'Cat young animal kitten',
     },
     {
       id: 11,
       img: 'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641__340.jpg',
-      title: 'dove bird flight wings.',
+      title: 'Dove bird flight wings.',
     },
     {
       id: 12,
       img: 'https://cdn.pixabay.com/photo/2013/11/01/11/13/dolphin-203875__340.jpg',
-      title: 'dolphin sea ocean',
+      title: 'Dolphin sea ocean',
     },
     {
       id: 13,
       img: 'https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453__340.jpg',
-      title: 'kingfisher bird',
+      title: 'Kingfisher bird',
     },
     {
       id: 14,
       img: 'https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954__340.jpg',
-      title: 'bird fluttering berries',
+      title: 'Bird fluttering berries',
     },
     {
       id: 15,
       img: 'https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658__340.jpg',
-      title: 'fox wildlife red fox',
+      title: 'Fox wildlife red fox',
     },
     {
       id: 16,
       img: 'https://cdn.pixabay.com/photo/2015/04/10/01/41/fox-715588__340.jpg',
-      title: 'fox nature animals',
+      title: 'Fox nature animals',
     },
     {
       id: 17,
       img: 'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg',
-      title: 'cat portrait',
+      title: 'Cat portrait',
     },
     {
       id: 18,
       img: 'https://cdn.pixabay.com/photo/2017/12/11/15/34/lion-3012515__340.jpg',
-      title: 'lion roar africa',
+      title: 'Lion roar africa',
     },
     {
       id: 19,
       img: 'https://cdn.pixabay.com/photo/2017/05/08/13/15/bird-2295436__340.jpg',
-      title: 'bird robin spring',
+      title: 'Bird robin spring',
     },
     {
       id: 20,
       img: 'https://cdn.pixabay.com/photo/2014/10/01/10/44/animal-468228__340.jpg',
-      title: 'hedgehog pet mammal',
+      title: 'Hedgehog pet mammal',
     },
     {
       id: 21,
       img: 'https://cdn.pixabay.com/photo/2017/02/28/23/00/swan-2107052__340.jpg',
-      title: 'swan water white',
+      title: 'Swan water white',
     },
     {
       id: 22,
@@ -128,48 +128,46 @@ export default function List() {
     {
       id: 23,
       img: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074__340.jpg',
-      title: 'puppy pet canine dog',
+      title: 'Puppy pet canine dog',
     },
     {
       id: 24,
       img: 'https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888__340.jpg',
-      title: 'tiger swamp big cat',
+      title: 'Tiger swamp big cat',
     },
     {
       id: 25,
       img: 'https://cdn.pixabay.com/photo/2017/10/25/16/54/african-lion-2888519__340.jpg',
-      title: 'african lion wild cat',
+      title: 'African lion wild cat',
     },
     {
       id: 26,
       img: 'https://cdn.pixabay.com/photo/2014/11/03/17/40/leopard-515509__340.jpg',
-      title: 'leopard safari mammal',
+      title: 'Leopard safari mammal',
     },
     {
       id: 27,
       img: 'https://cdn.pixabay.com/photo/2015/10/12/15/46/animal-984573__340.jpg',
-      title: 'mammal fallow deer stag',
+      title: 'Mammal fallow deer stag',
     },
     {
       id: 28,
       img: 'https://cdn.pixabay.com/photo/2018/01/22/14/13/animal-3099035__340.jpg',
-      title: 'horses fauna nature',
+      title: 'Horses fauna nature',
     },
     {
       id: 29,
       img: 'https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777__340.jpg',
-      title: 'elephant black-and-white',
+      title: 'Elephant black-and-white',
     },
     {
       id: 30,
       img: 'https://cdn.pixabay.com/photo/2016/08/14/17/04/ladybugs-1593406__340.jpg',
-      title: 'ladybugs insects pair',
+      title: 'Ladybugs insects pair',
     },
   ];
 
   const [search, setSearch] = useState('');
-
-  // console.log(data.filter(user => user.title.toLowerCase().includes('k')));
 
   return (
     <>
@@ -183,24 +181,43 @@ export default function List() {
           />
         </form>
         <div className="select-opt">
-          <select name="Sort" id="select" className="search-sort">
-            <option value="">Select</option>
-            <option value="Size">Image-Size</option>
-            <option value="Name">Name</option>
-          </select>
+          <form action="#">
+            <label htmlFor="select"></label>
+            <select name="Sort" id="select" className="search-sort">
+              <option value="" disabled>
+                Select
+              </option>
+              <option value="Size">Image-Size</option>
+              <option value="Name">Name</option>
+            </select>
+          </form>
         </div>
       </div>
-      <div className="main-img-container">
-        {data.filter(data=>data.title.toLowerCase().includes(search)).map(item => {
-          return (
-            <div className="img-container" key={item.id}>
-              <img src={item.img} alt="image 1" className="img" />
-              <p>
-                {item.title}
-              </p>
-            </div>
-          );
-        })}
+      <div className="grid-img-container">
+        {data
+          .filter(data => data.title.toLowerCase().includes(search))
+          .map(item => {
+            return (
+              <div className="grid-list-container" key={item.id}>
+                <img src={item.img} alt="image 1" className="grid-img" />
+                <p>{item.title}</p>
+              </div>
+            );
+          })}
+      </div>
+
+      <div className="list-img-container">
+        {data
+          .filter(data => data.title.toLowerCase().includes(search))
+          .map(item => {
+            return (
+              <div className="list-container" key={item.id}>
+                <img src={item.img} alt="image 1" className="list-img" />
+                  <h2>{item.title}</h2>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati omnis dolores quia culpa rerum, harum, aliquid magnam perspiciatis sit explicabo repudiandae vero recusandae! Cum, sapiente! In enim dignissimos eum eligendi.</p>
+              </div>
+            );
+          })}
       </div>
     </>
   );
