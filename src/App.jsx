73aa121e6b0a759e9de321bed3bdfda7295f8ec4
Abhilash4597 <abhilash4597@gmail.com>
2023-04-01 17:35:53 +0530

    folder structure update

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import './App.css'
 import List from './components/list'
 import Header from './components/header';
 import Footer from './components/footer';
+import data from './components/data';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header></Header>
-      <List></List>
-      <Footer></Footer>
+      <Header />
+      <List data={data} />
+      <Footer />
     </>
   );
 }
